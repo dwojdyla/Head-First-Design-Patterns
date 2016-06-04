e4cc47c3ec4f9e7161cb6b49cf4c56bb54bfa001
dwojdyla-tgm, Dominik Wojdyla <dwojdyla@student.tgm.ac.at>
2016-06-04 22:30:21 +0200

    Updated Javadoc

--- src/headfirst/designpatterns/combining/observer/RubberDuck.java
+++ src/headfirst/designpatterns/combining/observer/RubberDuck.java
@@ -1,25 +1,42 @@
 package headfirst.designpatterns.combining.observer;
-
+/**
+ * @author: Dominik Wojdyla
+ * @04.06.2016
+ * 
+ * Die Klasse implementiert Quackable 
+ * 
+ * */
 public class RubberDuck implements Quackable {
 	Observable observable;
-
+/**
+ * Neue Instanz wird erstellt
+ * */
 	public RubberDuck() {
 		observable = new Observable(this);
 	}
- 
+ /**
+  * Methode hat eine Ausgabe "Squeak"
+  * Eine Methode wird ausgeführt 
+  * */
 	public void quack() {
 		System.out.println("Squeak");
 		notifyObservers();
 	}
-
+/**
+ * Eine Methode von observable wird ausgeführt
+ * */
 	public void registerObserver(Observer observer) {
 		observable.registerObserver(observer);
 	}
-
+/**
+ * Eine Methode von observable wird ausgeführt
+ * */
 	public void notifyObservers() {
 		observable.notifyObservers();
 	}
-  
+  /**
+   * toString Methode
+   * */
 	public String toString() {
 		return "Rubber Duck";
 	}
