43f69b75572421fd435534720ce45bfbc0dbda59
dwojdyla-tgm, Dominik Wojdyla <dwojdyla@student.tgm.ac.at>
2016-06-04 22:06:42 +0200

    Updated Javadoc

--- src/headfirst/designpatterns/combining/observer/GooseAdapter.java
+++ src/headfirst/designpatterns/combining/observer/GooseAdapter.java
@@ -1,27 +1,46 @@
 package headfirst.designpatterns.combining.observer;
-
+/**
+ * @author: Dominik Wojdyla
+ * @04.06.2016 
+ * 
+ * Die Klasse implementiert Interface Quackable
+ * Adapter Pattern(Hüllenklasse)
+ * 
+ * */
 public class GooseAdapter implements Quackable {
 	Goose goose;
 	Observable observable;
-
+/**
+ * Konstruktor
+ * neue Instanz von observable wird erstellt 
+ * */
 	public GooseAdapter(Goose goose) {
 		this.goose = goose;
 		observable = new Observable(this);
 	}
- 
+ /**
+  * Methode von goose wird aufgerufen
+  * anschließend wird notifyObservers aufgerufen
+  * */
 	public void quack() {
 		goose.honk();
 		notifyObservers();
 	}
-
+/**
+ * Methode von Observable wird aufgerufen
+ * */
 	public void registerObserver(Observer observer) {
 		observable.registerObserver(observer);
 	}
-
+/**
+ * Methode von observable wird aufgerufen
+ * */
 	public void notifyObservers() {
 		observable.notifyObservers();
 	}
-
+/**
+ * toString Methode 
+ * */
 	public String toString() {
 		return "Goose pretending to be a Duck";
 	}
