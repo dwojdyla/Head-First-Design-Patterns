e3799bf1d1fdd6dc132ae2996fb9aadfa48cfecd
dwojdyla-tgm, Dominik Wojdyla <dwojdyla@student.tgm.ac.at>
2016-06-04 21:22:19 +0200

    Updated Javadoc

--- src/headfirst/designpatterns/combining/observer/DuckCall.java
+++ src/headfirst/designpatterns/combining/observer/DuckCall.java
@@ -1,25 +1,44 @@
 package headfirst.designpatterns.combining.observer;
 
+/**
+ * @author Dominik Wojdyla
+ * @04.06.2016
+ * 
+ * Die Klasse implementiert das Interface Quackable
+ * 
+ */
 public class DuckCall implements Quackable {
 	Observable observable;
-
+/*
+*Neue Instanz von Observable
+*/
 	public DuckCall() {
 		observable = new Observable(this);
 	}
- 
+ /*
+ *Erstmals eine Ausgabe "Kwak".
+ *dann Ausführung der Methode
+ */
 	public void quack() {
 		System.out.println("Kwak");
 		notifyObservers();
 	}
- 
+ /*
+ *Eine Methode von observable wird aufgerufen
+ */
 	public void registerObserver(Observer observer) {
 		observable.registerObserver(observer);
 	}
-
+/*
+*Methode von Observable wird aufgerufen
+*/
 	public void notifyObservers() {
 		observable.notifyObservers();
 	}
- 
+/**
+ *Ein String wird zurückgegeben 
+ * 
+ */
 	public String toString() {
 		return "Duck Call";
 	}
