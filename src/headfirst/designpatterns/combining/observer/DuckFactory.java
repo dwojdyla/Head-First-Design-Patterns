4b969b3070c1c27a60aa550c96752f9edf6ffbf0
dwojdyla-tgm, Dominik Wojdyla <dwojdyla@student.tgm.ac.at>
2016-06-04 21:28:29 +0200

    Updated Javadoc

--- src/headfirst/designpatterns/combining/observer/DuckFactory.java
+++ src/headfirst/designpatterns/combining/observer/DuckFactory.java
@@ -1,7 +1,20 @@
 package headfirst.designpatterns.combining.observer;
-
+/**
+ *@author: Dominik Wojdyla
+ *@04.06.2016
+ * 
+ * Die Klasse erbt von einer abstrakten Klasse.
+ *
+ */
 public class DuckFactory extends AbstractDuckFactory {
-  
+  /**
+   * Bei jeder Methode wird eine neue Instanz einer Klasse erzeugt.
+   * 1.Methode Instant von MallardDuck
+   * 2.Methode Instanz von RedheadDuck
+   * 3.Methode Instanz von DuckCall
+   * 4.Methode RubberDuck
+   * 
+   * */
 	public Quackable createMallardDuck() {
 		return new MallardDuck();
 	}
