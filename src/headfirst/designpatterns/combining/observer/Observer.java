2693c84378fc41616e83b483a7ba509cda42171e
dwojdyla-tgm, Dominik Wojdyla <dwojdyla@student.tgm.ac.at>
2016-06-04 22:17:12 +0200

    Updated Javadoc

--- src/headfirst/designpatterns/combining/observer/Observer.java
+++ src/headfirst/designpatterns/combining/observer/Observer.java
@@ -1,5 +1,11 @@
 package headfirst.designpatterns.combining.observer;
-
+/**
+ * @author: Dominik Wojdyla
+ * @04.06.2016
+ * 
+ * In dieser Klasse haben wir eine Methode update.
+ * In dieser wird durch QuackObservable des typs duck definiert
+ * */
 public interface Observer {
 	public void update(QuackObservable duck);
 }
