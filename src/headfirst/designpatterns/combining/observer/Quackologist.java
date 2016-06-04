9b44f26a317c83d0052833f41d523a232127b3b0
dwojdyla-tgm, Dominik Wojdyla <dwojdyla@student.tgm.ac.at>
2016-06-04 22:25:11 +0200

    Updated Javadoc

--- src/headfirst/designpatterns/combining/observer/Quackologist.java
+++ src/headfirst/designpatterns/combining/observer/Quackologist.java
@@ -1,11 +1,21 @@
 package headfirst.designpatterns.combining.observer;
-
+/**
+ * @author: Dominik Wojdyla
+ * @04.06.2016
+ * 
+ * Die Klasse implementiert Observer
+ *
+ * */
 public class Quackologist implements Observer {
- 
+ /**
+  * EIne Methode update, welche eun Ausgabe macht.
+  * */
 	public void update(QuackObservable duck) {
 		System.out.println("Quackologist: " + duck + " just quacked.");
 	}
- 
+ /**
+  * toString Methode 
+  * */
 	public String toString() {
 		return "Quackologist";
 	}
