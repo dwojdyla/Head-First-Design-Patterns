a8c42f244c9451c0ad712d3a9a974b1eaf8d137a
dwojdyla-tgm, Dominik Wojdyla <dwojdyla@student.tgm.ac.at>
2016-06-04 20:37:45 +0200

    Updated Javadoc

--- src/headfirst/designpatterns/combining/observer/CountingDuckFactory.java
+++ src/headfirst/designpatterns/combining/observer/CountingDuckFactory.java
@@ -1,7 +1,21 @@
 package headfirst.designpatterns.combining.observer;
-
+/**
+ * 
+ * @author Dominik Wojdyla
+ * @04.06.2016
+ * 
+ * Die Klasse erbt von der Klasse AbstractDuckFactory
+ * Sie beinhaltet 4 Methoden
+*/
 public class CountingDuckFactory extends AbstractDuckFactory {
   
+  /*
+  *Jede der 4 Methoden ruft erst die Quackcounter Klasse auf und asnchließend wird eine neuenstanz erstellt.
+  *1.Methode neue Mallarduck 
+  *2.Methode neue RedheadDuck
+  *3.Methode neue Duckcall
+  *4.Methode neue RubberDuck
+  */
 	public Quackable createMallardDuck() {
 		return new QuackCounter(new MallardDuck());
 	}
