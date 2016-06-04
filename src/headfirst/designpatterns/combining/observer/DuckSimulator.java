66e7a02901af2eb4a5698a2b0c696badba160583
dwojdyla-tgm, Dominik Wojdyla <dwojdyla@student.tgm.ac.at>
2016-06-04 21:37:50 +0200

    Update Jacadoc

--- src/headfirst/designpatterns/combining/observer/DuckSimulator.java
+++ src/headfirst/designpatterns/combining/observer/DuckSimulator.java
@@ -1,15 +1,37 @@
 package headfirst.designpatterns.combining.observer;
+/**
+ * @author: Dominik Wojdyla
+ * @04.06.2016
+ * 
+ * Das ist die "Hauptklasse".
+ * Hier befindet sich die Main und das Programm wir von dieser Klasse aus gestartet
+ * 
+ * */
 
 public class DuckSimulator {
+	/**
+	 * Main
+	 * */
 	public static void main(String[] args) {
 		DuckSimulator simulator = new DuckSimulator();
 		AbstractDuckFactory duckFactory = new CountingDuckFactory();
  
 		simulator.simulate(duckFactory);
 	}
-  
+	
+  /**
+   * Ruft folgende Methoden auf
+   * */
 	void simulate(AbstractDuckFactory duckFactory) {
-  
+  /**
+   * Es wird erstellt:
+   * -redheadDuck
+   * -duckCall
+   * -rubberDuck
+   * -gooseDuck
+   * 
+   * -Flock
+   * */
 		Quackable redheadDuck = duckFactory.createRedheadDuck();
 		Quackable duckCall = duckFactory.createDuckCall();
 		Quackable rubberDuck = duckFactory.createRubberDuck();
@@ -17,25 +39,34 @@ public class DuckSimulator {
  
 		Flock flockOfDucks = new Flock();
  
+ /**
+  * Die erstellten Sachen werden dem Flock geaddet.
+  * 
+  * */
 		flockOfDucks.add(redheadDuck);
 		flockOfDucks.add(duckCall);
 		flockOfDucks.add(rubberDuck);
 		flockOfDucks.add(gooseDuck);
- 
+//Wieder ein Flock erstellt 
 		Flock flockOfMallards = new Flock();
  
+ /**
+  * Es werden 4 MallardDucks erstellt.
+  * */
 		Quackable mallardOne = duckFactory.createMallardDuck();
 		Quackable mallardTwo = duckFactory.createMallardDuck();
 		Quackable mallardThree = duckFactory.createMallardDuck();
 		Quackable mallardFour = duckFactory.createMallardDuck();
-
+//Diese werden dem erstellten Flock geaddet
 		flockOfMallards.add(mallardOne);
 		flockOfMallards.add(mallardTwo);
 		flockOfMallards.add(mallardThree);
 		flockOfMallards.add(mallardFour);
 
 		flockOfDucks.add(flockOfMallards);
-
+/**
+ * Ausgabe 
+ * */
 		System.out.println("\nDuck Simulator: With Observer");
 
 		Quackologist quackologist = new Quackologist();
