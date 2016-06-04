95f08737b150f7cf6f832487a926b8a07bfd010c
dwojdyla-tgm, Dominik Wojdyla <dwojdyla@student.tgm.ac.at>
2016-06-04 21:59:34 +0200

    Updated Javadoc

--- src/headfirst/designpatterns/combining/observer/Goose.java
+++ src/headfirst/designpatterns/combining/observer/Goose.java
@@ -1,11 +1,21 @@
 package headfirst.designpatterns.combining.observer;
-
+/**
+ * @author: Dominik Wojdyla
+ * @04.06.2016
+ * 
+ * Allgemein Adapter Pattern (Hüllenklasse)
+ * */
 public class Goose {
-
+/**
+ * Einfach Ausgabe "Honk"
+ * */
 	public void honk() {
 		System.out.println("Honk");
 	}
 
+/**
+ * toString Methdode
+ * */
 	public String toString() {
 		return "Goose";
 	}
