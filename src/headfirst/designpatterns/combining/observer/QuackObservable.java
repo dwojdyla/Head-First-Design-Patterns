c6c794198f767abf69d0025f557c06908a2ce09b
dwojdyla-tgm, Dominik Wojdyla <dwojdyla@student.tgm.ac.at>
2016-06-04 22:22:27 +0200

    Updated Javadoc

--- src/headfirst/designpatterns/combining/observer/QuackObservable.java
+++ src/headfirst/designpatterns/combining/observer/QuackObservable.java
@@ -1,5 +1,12 @@
 package headfirst.designpatterns.combining.observer;
-
+/**
+ * @author: Dominik Wojdyla
+ * @04.06.2016
+ * 
+ * Es handelt sich um ein Interface, mit 2 Methoden:
+ * -registerObserver
+ * -notifyObservers
+ * */
 public interface QuackObservable {
 	public void registerObserver(Observer observer);
 	public void notifyObservers();
