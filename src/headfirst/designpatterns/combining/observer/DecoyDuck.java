842a73893a8b63c2b43b03c7a6d8c386d63e4838
dwojdyla-tgm, Dominik Wojdyla <dwojdyla@student.tgm.ac.at>
2016-06-04 20:46:48 +0200

    Updated Javadoc

--- src/headfirst/designpatterns/combining/observer/DecoyDuck.java
+++ src/headfirst/designpatterns/combining/observer/DecoyDuck.java
@@ -1,25 +1,43 @@
 package headfirst.designpatterns.combining.observer;
-
+/**
+ * 
+ * @author Dominik Wojdyla
+ * @04.06.2016
+ * 
+ * Die Klasse implementiert das Interface Quackable.
+ * Es hat somit alle Methoden von Quackable. 
+ * 
+ * */
 public class DecoyDuck implements Quackable {
 	Observable observable;
 
+/*Hier wird eine neue Instanz von observable erstellt.
+*/
 	public DecoyDuck() {
 		observable = new Observable(this);
 	}
- 
+ /*
+ *Als erstes eine Ausgabe und anschließend wird die angegebene Methode aufgerufen 
+ */
 	public void quack() {
 		System.out.println("<< Silence >>");
 		notifyObservers();
 	}
- 
+ /*
+ *Es wird eine Methode von observable aufgerufen
+ */
 	public void registerObserver(Observer observer) {
 		observable.registerObserver(observer);
 	}
-
+/*
+*Es wird eine Methode von observable aufgerufen 
+*/
 	public void notifyObservers() {
 		observable.notifyObservers();
 	}
- 
+ /*
+ *Die Methode gibt einen String aus
+ */
 	public String toString() {
 		return "Decoy Duck";
 	}
