676fcc93e68db9862d9be42ea76454045795d9e0
dwojdyla-tgm, Dominik Wojdyla <dwojdyla@student.tgm.ac.at>
2016-06-04 22:10:01 +0200

    Updated Javadoc

--- src/headfirst/designpatterns/combining/observer/MallardDuck.java
+++ src/headfirst/designpatterns/combining/observer/MallardDuck.java
@@ -1,25 +1,44 @@
 package headfirst.designpatterns.combining.observer;
 
+/**
+ * @author: Dominik Wojdyla
+ * @04.06.2016
+ * 
+ * Die Klasse implementiert das Interface Quackable
+ * Hat somit alle Methoden des Interfaces
+ * 
+ * */
 public class MallardDuck implements Quackable {
 	Observable observable;
- 
+/**
+ * neue Instanz von observable wird erstellt
+ **/ 
 	public MallardDuck() {
 		observable = new Observable(this);
 	}
- 
+ /**
+  * Ausgabe "Quack"
+  * notifyObservers Methode wird aufgerufen
+  * */ 
 	public void quack() {
 		System.out.println("Quack");
 		notifyObservers();
 	}
- 
+ /**
+  * Methode von observable wird aufgerufen
+  * */
 	public void registerObserver(Observer observer) {
 		observable.registerObserver(observer);
 	}
- 
+ /**
+  * methode von observable wird aufgerufen
+  * */
 	public void notifyObservers() {
 		observable.notifyObservers();
 	}
- 
+ /**
+  * toString Methode
+  * */
 	public String toString() {
 		return "Mallard Duck";
 	}
