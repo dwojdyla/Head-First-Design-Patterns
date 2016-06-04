f575f6ab1230c3eb713ba48dc718e27dd03728f7
dwojdyla-tgm, Dominik Wojdyla <dwojdyla@student.tgm.ac.at>
2016-06-04 20:33:04 +0200

    Updated Javadoc

--- src/headfirst/designpatterns/combining/observer/AbstractDuckFactory.java
+++ src/headfirst/designpatterns/combining/observer/AbstractDuckFactory.java
@@ -1,5 +1,12 @@
 package headfirst.designpatterns.combining.observer;
-
+/**
+ * @author Dominik Wojdyka
+ * @04.06.2016
+ * 
+ * Factory Pattern
+ * Die Klasse ist abstract und hat 4 Methoden, welche auch abstract sind. 
+ * 
+ **/
 public abstract class AbstractDuckFactory {
  
 	public abstract Quackable createMallardDuck();
