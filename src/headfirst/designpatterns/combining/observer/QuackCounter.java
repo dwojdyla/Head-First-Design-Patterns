cd80e78805baa81300d4edbdf4a46cc1a44c9e9e
dwojdyla-tgm, Dominik Wojdyla <dwojdyla@student.tgm.ac.at>
2016-06-04 22:21:12 +0200

    Updated Javadoc

--- src/headfirst/designpatterns/combining/observer/QuackCounter.java
+++ src/headfirst/designpatterns/combining/observer/QuackCounter.java
@@ -1,5 +1,11 @@
 package headfirst.designpatterns.combining.observer;
-
+/**
+ * @author: Dominik Wojdyla
+ * @04.06.2016
+ * 
+ * Klasse implementiert Interface Quackable, somit hat die Klasse
+ * alle Methoden des Interfaces
+ * */
 public class QuackCounter implements Quackable {
 	Quackable duck;
 	static int numberOfQuacks;
@@ -7,24 +13,35 @@ public class QuackCounter implements Quackable {
 	public QuackCounter(Quackable duck) {
 		this.duck = duck;
 	}
-  
+  /**
+   * Hier wird die Methode quack() von duck aufgerufen 
+   * anschließend wird die oben definierte numberOfQuacks erhöht.
+   * */
 	public void quack() {
 		duck.quack();
 		numberOfQuacks++;
 	}
- 
+ /**
+  *getter Methode wo die int numberOfQuacks zurückgegeben wird. 
+  * */ 
 	public static int getQuacks() {
 		return numberOfQuacks;
 	}
- 
+ /**
+  * Aufruf einer Methode von duck
+  * */
 	public void registerObserver(Observer observer) {
 		duck.registerObserver(observer);
 	}
- 
+ /**
+  * Aufruf einer notufy Methode con duck 
+  * */
 	public void notifyObservers() {
 		duck.notifyObservers();
 	}
-   
+   /**
+    * toString Methode
+    * */
 	public String toString() {
 		return duck.toString();
 	}
