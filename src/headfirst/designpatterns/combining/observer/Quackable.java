d66703b2aaa0a8c18362451ded0257e253274035
dwojdyla-tgm, Dominik Wojdyla <dwojdyla@student.tgm.ac.at>
2016-06-04 22:23:47 +0200

    Updated Javadoc

--- src/headfirst/designpatterns/combining/observer/Quackable.java
+++ src/headfirst/designpatterns/combining/observer/Quackable.java
@@ -1,5 +1,13 @@
 package headfirst.designpatterns.combining.observer;
-
+/**
+ * @author: Dominik Wojdyla
+ * @04.06.2016
+ * 
+ * Es handelt sich um ein Interface welches von Quackobservable erbt.
+ * Interface ist nur ein Bauplan
+ * Das Interface hat eine Methode:
+ * -quack()
+ * */
 public interface Quackable extends QuackObservable {
 	public void quack();
 }
